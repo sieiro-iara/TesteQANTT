6d093101d505b7c1759e035af33afb45a53abaab
sieiro-iara <iarasieiro@gmail.com>
2025-04-23 17:07:10 -0300

    Criação cenários de teste cypress frontend

---
+++ cypress/e2e/Cenario2_Login.cy.js
@@ -0,0 +1,41 @@
+describe('template spec', () => {
+//Login de usuario comum
+it('passes', () => {
+    cy.visit('https://front.serverest.dev/login')
+    cy.get('[data-testid="email"]').click().type("testentt003@yopmail.com")
+    cy.get('[data-testid="senha"]').click().type("Teste@01")
+    cy.get('[data-testid="entrar"]').click()
+    cy.wait(5000)
+    cy.url().should('eq', 'https://front.serverest.dev/home')
+  })
+
+    //Login de usuario admin
+    it('passes', () => {
+      cy.visit('https://front.serverest.dev/login')
+      cy.get('[data-testid="email"]').click().type("testentt004@yopmail.com")
+      cy.get('[data-testid="senha"]').click().type("Teste@01")
+      cy.get('[data-testid="entrar"]').click()
+      cy.wait(5000)
+      cy.url().should('eq', 'https://front.serverest.dev/admin/home')
+    })
+
+    //Erro ao logar
+    it('passes', () => {
+        cy.visit('https://front.serverest.dev/login')
+        cy.get('[data-testid="entrar"]').click()
+        cy.wait(5000)
+        cy.contains('Email é obrigatório').should('be.visible')
+        cy.contains('Password é obrigatório').should('be.visible')
+      })
+
+    
+    //Erro ao logar
+    it('passes', () => {
+        cy.visit('https://front.serverest.dev/login')
+        cy.get('[data-testid="email"]').click().type("testentt004@yopmail.com")
+        cy.get('[data-testid="senha"]').click().type("Teste@02")
+        cy.get('[data-testid="entrar"]').click()
+        cy.wait(5000)
+        cy.contains('Email e/ou senha inválidos').should('be.visible')
+      })
+})
---
+++ cypress/e2e/Cenario3_Carrinho.cy.js
@@ -0,0 +1,49 @@
+    describe('template spec', () => {
+    //Comprar um item
+  it('passes', () => {
+    cy.visit('https://front.serverest.dev/login')
+    cy.get('[data-testid="email"]').click().type("testentt111@yopmail.com")
+    cy.get('[data-testid="senha"]').click().type("Teste@01")
+    cy.get('[data-testid="entrar"]').click()
+    cy.url().should('eq', 'https://front.serverest.dev/home')
+    cy.get(':nth-child(1) > .card-body > div > [href="/minhaListaDeProdutos"] > [data-testid="adicionarNaLista"]').click()
+    cy.get('[data-testid="adicionar carrinho"]').click()
+    cy.url().should('eq', 'https://front.serverest.dev/carrinho') 
+  })
+  
+  //Comprar multiplos itens
+  it('passes', () => {
+    cy.visit('https://front.serverest.dev/login')
+    cy.get('[data-testid="email"]').click().type("testentt111@yopmail.com")
+    cy.get('[data-testid="senha"]').click().type("Teste@01")
+    cy.get('[data-testid="entrar"]').click()
+    cy.url().should('eq', 'https://front.serverest.dev/home')
+    cy.get(':nth-child(1) > .card-body > div > [href="/minhaListaDeProdutos"] > [data-testid="adicionarNaLista"]').click()
+    cy.get('[data-testid="product-increase-quantity"]').click()
+    cy.contains('Preço R$200').should('be.visible')
+    cy.contains('Total: 2').should('be.visible')
+    cy.get('[data-testid="product-increase-quantity"]').click()
+    cy.contains('Preço R$300').should('be.visible')
+    cy.contains('Total: 3').should('be.visible')
+    cy.get('[data-testid="adicionar carrinho"]').click()
+    cy.url().should('eq', 'https://front.serverest.dev/carrinho') 
+  })
+
+  //Limpar lista de compras
+  it('passes', () => {
+    cy.visit('https://front.serverest.dev/login')
+    cy.get('[data-testid="email"]').click().type("testentt111@yopmail.com")
+    cy.get('[data-testid="senha"]').click().type("Teste@01")
+    cy.get('[data-testid="entrar"]').click()
+    cy.url().should('eq', 'https://front.serverest.dev/home')
+    cy.get(':nth-child(1) > .card-body > div > [href="/minhaListaDeProdutos"] > [data-testid="adicionarNaLista"]').click()
+    cy.get('[data-testid="product-increase-quantity"]').click()
+    cy.contains('Preço R$200').should('be.visible')
+    cy.contains('Total: 2').should('be.visible')
+    cy.get('[data-testid="product-increase-quantity"]').click()
+    cy.contains('Preço R$300').should('be.visible')
+    cy.contains('Total: 3').should('be.visible')
+    cy.get('[data-testid="limparLista"]').click()
+    cy.contains('Seu carrinho está vazio').should('be.visible')
+  })
+})
